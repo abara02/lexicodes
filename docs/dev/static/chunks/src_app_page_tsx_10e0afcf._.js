17d5fb129028479327eb40140af3c6fcbd6aae67
abara02 <alexisbaranauskas@gmail.com>
2026-01-28 19:32:53 -0500

    Fix Vercel deployment by removing distDir override and legacy export scripts

--- docs/dev/static/chunks/src_app_page_tsx_10e0afcf._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_74363266._.js",
-  "static/chunks/node_modules_bfdaf397._.js"
-],
-    source: "dynamic"
-});
